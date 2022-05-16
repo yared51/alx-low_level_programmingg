1def72fa535af16f87f7806744d7e4908ad47df7
yared51 <yared.birhan51@gmail.com>
2022-05-16 22:06:08 +0300

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,7 @@
  *to make change for an amount of money.
  *@argc:number of commandline arguments.
  *@argv:pointer to an array of commmand line arguments.
- *Return:0-success, non-zero-fail.
+ *Return:0-success, non-zero-fail. -
  */
 
 int main(int argc, char *argv[])
