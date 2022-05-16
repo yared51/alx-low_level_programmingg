a65d4c63967c875ab50fef70997e2d4ee767a88f
yared51 <yared.birhan51@gmail.com>
2022-05-16 22:01:43 +0300

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,8 +4,8 @@
 /**
  * main - Program that takes first two integer arguments and prints the product
  * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if not enough arguments passed in, 0 otherwise
+ * @argv: Array name 
+ * Return: 1 if not enough arguments passed in, 0 otherwise --
  */
 
 int main(int argc, char *argv[])
